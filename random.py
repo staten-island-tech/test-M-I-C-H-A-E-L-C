46df0dbd2dcba6cc9f8431da2dff8e2156dbf110
M-I-C-H-A-E-L-C <michaelc1242@nycstudents.net>
2026-01-29 10:18:06 -0500

    Day 2

---
+++ random.py
@@ -0,0 +1,11 @@
+import turtle
+from turtle import *
+t = Turtle()
+
+
+t.speed(100)
+
+
+for i in range(200000000):
+    t.forward(200)
+    t.left(91)
--- test.py
+++ test.py
@@ -16,5 +16,19 @@ def square(x):
     t.left(90)
 
 square(200)
+""" x = [1]
+print(x) """
+
+
+import random 
+
+random_integer = random.int(1,10)
+
+print(f"random integer between 1 and 10: {random_integer}")
+
+
+
+
+
 
 turtle.done
